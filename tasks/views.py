3303f0cf8a99743ef5ba62321f033c50e9979f8f
Léo <leogv77@gmail.com>
2025-10-21 11:12:43 +0200

    Rendre l’API exploitable (filtres + tri + recherche)

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,18 @@
 from rest_framework.viewsets import ModelViewSet 
 from .models import Task 
 from .serializers import TaskSerializer
+from rest_framework import filters, pagination
+from django_filters.rest_framework import DjangoFilterBackend
+
+
+class TaskPagination(pagination.PageNumberPagination):
+    page_size = 5
 
 class TaskViewSet(ModelViewSet):
     queryset = Task.objects.all().order_by('-id')
     serializer_class = TaskSerializer
+    pagination_class = TaskPagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['status']
+    search_fields = ['title', 'status']
+    ordering_fields = ['id', 'title', 'status', 'created_at']
