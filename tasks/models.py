0737a93cd70d2b68fd3066c819dde5c8a1f02a10
Léo <leogv77@gmail.com>
2025-10-21 10:19:50 +0200

    Introduction validation métier

--- tasks/models.py
+++ tasks/models.py
@@ -1,8 +1,19 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+
+def validate_status(value):
+    if value not in ["A faire", "En cours", "Fait"]:
+        raise ValidationError(f'Statut invalide, veuillez choisir : "A faire", "En cours", "Fait"')
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    status = models.CharField(max_length=20, default="A faire")
+    status = models.CharField(
+        max_length=20,
+        default="A faire",
+        validators=[validate_status],
+    )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
